55f2064195fce53132a59eca94b8d01f3afb216f
Cam <cammclain@protonmail.com>
2025-01-05 16:09:44 -0600

    Refactor core application structure by moving User model and database setup
    - Deleted the old models.py file and moved the User model definition to a new models.py file under the database/models directory.
    - Created new init files for the crud and database directories to establish package structure.
    - Introduced a new db.py file for database engine and session management.
    - Updated the overall organization of the core application for better modularity and maintainability.

---
+++ src/teamserver/packages/core/src/core/application/database/db.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Boolean, Column, Integer, String, create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+Base = declarative_base()
+
+engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
