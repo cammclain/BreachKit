9f090098e0a0a4deda2c2369a9fe9f6d0ab6ee64
Cam <cammclain@protonmail.com>
2025-01-05 16:10:08 -0600

    Add __repr__ method to User model for improved string representation

--- src/teamserver/packages/core/src/core/application/database/models/models.py
+++ src/teamserver/packages/core/src/core/application/database/models/models.py
@@ -10,4 +10,5 @@ class User(Base):
     is_active = Column(Boolean, default=True)
     is_superuser = Column(Boolean, default=False)
 
-
+    def __repr__(self):
+        return f"User(id={self.id}, username={self.username}, is_active={self.is_active}, is_superuser={self.is_superuser})"
