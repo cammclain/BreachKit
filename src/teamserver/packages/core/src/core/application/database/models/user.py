e49f8cc50692dd35afcc3b57313956f3cf2587d2
Cam <cammclain@protonmail.com>
2025-01-05 19:06:00 -0600

    Add Objective, Campaign, and Goal models to database schema
    - Introduced new models: Objective, Campaign, and Goal in objective.py, defining their structure and relationships within the database.
    - Established foreign key relationships to enhance data integrity and organization.
    - Updated the User model in user.py to include relationships with Campaign and Objective models.
    - Implemented timestamps for creation and updates to track model changes effectively.
    - This addition improves the overall database schema and supports future functionality related to campaign and goal management.

--- src/teamserver/packages/core/src/core/application/database/models/user.py
+++ src/teamserver/packages/core/src/core/application/database/models/user.py
@@ -1,9 +1,9 @@
 from ..db import Base
-from enum import Enum
-from sqlalchemy import Boolean, Column, Integer, String, DateTime
+from sqlalchemy import Boolean, Column, Integer, String, DateTime, Enum
 from datetime import datetime
-
-
+from .objective import Campaign, Objective, Goal
+from .loot import Loot, Credential
+from sqlalchemy.orm import relationship
 class UserRole(Enum):
     ADMIN: str = "admin"
     USER: str = "user"
@@ -22,7 +22,11 @@ class Team(Base):
     description = Column(String)
     created_at = Column(DateTime, default=datetime.now)
     updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    campaigns = relationship("Campaign", back_populates="team")
+    users = relationship("User", back_populates="team")
 
+    def __repr__(self):
+        return f"Team(id={self.id}, name={self.name}, description={self.description}, created_at={self.created_at}, updated_at={self.updated_at})"
 
 class User(Base):
     __tablename__ = "users"
