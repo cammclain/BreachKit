2478ca1ab1b09a708a4dd54f3798d09e70a9083b
Cam <cammclain@protonmail.com>
2025-01-09 01:15:30 -0600

    Add Windows model for Active Directory integration
    - Introduced a new `windows.rs` file containing SurrealDB schemas for objects/observables in a Windows environment within Active Directory.
    - Defined `Hostname` and `Host` structs to represent machine hostnames and Windows hosts, respectively, enhancing the server's capability to manage and interact with Windows-specific data.
    - This addition lays the groundwork for further integration of Windows environment data into the server application.

---
+++ src/teamserver/packages/server/src/application/models/windows.rs
@@ -0,0 +1,20 @@
+// This file contains the SurrealDB schema's for objects/observables that can be found in a Windows environment, within a larger Active Directory Environment
+
+use chrono::{Datetime, Utc};
+use serde::{Deserialize, Serialize};
+use surrealdb::sql::Thing;
+
+use surrealdb::RecordId;
+
+// This model represents a hostname that a machine within the Active Directory environment has
+#[derive(Debug, Serialize, Deserialize)]
+struct Hostname<'a> {
+    hostname_value: &'a str,
+}
+
+// This model represents a Windows host within an Active Directory environment
+#[derive(Debug, Serialize)]
+struct Host<'a> {
+    id: RecordId,
+    hostname: Hostname<'a>,
+}
