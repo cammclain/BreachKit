a11813afb89f679fc8e7376cc17da5a154c0b369
Cam <cammclain@protonmail.com>
2025-01-09 02:15:17 -0600

    Refactor main function in main.rs for application launch
    - Updated the `main.rs` file to include a detailed comment explaining its role in launching the application and pulling in functionality from the application module.
    - Converted the `main` function to an asynchronous function using `tokio`, enhancing the application's capability to handle asynchronous operations.
    - This change improves code clarity and sets the stage for better integration of asynchronous features in the server application.

--- src/teamserver/packages/server/src/main.rs
+++ src/teamserver/packages/server/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
+// This is of course the main.rs for the application, and handles the ove3rall "launching" of the application.
+// It will pull in all the functionality of the application from the application module.
+
+use serde::{Deserialize, Serialize};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!");
 }
