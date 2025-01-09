26fb819b2cc13cf32ef8927f6dcb317579a6d8d1
Cam <cammclain@protonmail.com>
2025-01-09 02:24:01 -0600

    Enhance common.rs with User and Credential models for BreachKit framework
    - Updated the common.rs file to include a new Credential struct alongside the existing User struct.
    - The Credential struct defines the SurrealDB schema for storing credential information, including username and password.
    - This addition improves the data model for the BreachKit framework, facilitating better management of user and credential data.

--- src/teamserver/packages/server/src/application/models/common.rs
+++ src/teamserver/packages/server/src/application/models/common.rs
@@ -1,11 +1,12 @@
-// This file contains thw surrealDB schemas for data that the agent can collect from windows environments within larger Active Directory environments.
-
+// This file contains the SurrealDB schema's for objects/observables that are a part of the framework itself
+// It also contains the 
 use serde::{Deserialize, Serialize};
 
 use chrono::{Datetime, Utc};
 
 use surrealdb::sql::Thing;
 
+// This model represents a user/operator within the BreachKit framework
 #[derive(Debug, Serialize, Deserialize)]
 pub struct User {
     pub id: Option<Thing>,     // SurrealDB record ID
@@ -16,3 +17,13 @@ pub struct User {
     pub credentials: Vec<Thing>, // IDs of the related credentials? NOTE: I am honestly not sure if I want to link "credentials" to the campagin or like the actual operation
     pub last_login: Option<DateTime<Utc>>, // last login timestamp
 }
+
+
+// This model represents a credential within the BreachKit framework
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Credential {
+    pub id: Option<Thing>, // SurrealDB record ID
+    pub username: String, // the username of the credential
+    pub password: String, // the password of the credential
+}
+        
