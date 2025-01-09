1bcec0f01bdedb465c613b0dda46769019c1aaae
Cam <cammclain@protonmail.com>
2025-01-09 02:24:07 -0600

    Add Domain and Credential models for Active Directory integration

--- src/teamserver/packages/server/src/application/models/active_directory.rs
+++ src/teamserver/packages/server/src/application/models/active_directory.rs
@@ -1,1 +1,16 @@
 // This file is for the SurrealDB database models/schema for the various Active Directory observables that the agents within the BreachKit framework can collect
+
+// This model represents a Domain within the BreachKit framework
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Domain {
+    pub id: Option<Thing>, // SurrealDB record ID
+    pub name: String, // the name of the domain
+}
+
+// This model represents a credential within the BreachKit framework
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Credential {
+    pub id: Option<Thing>, // SurrealDB record ID
+    pub username: String, // the username of the credential
+    pub password: String, // the password of the credential
+}
