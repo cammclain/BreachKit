0722af9c94057e5fb07e49c9c779a61b122f8a1e
Cam <cammclain@protonmail.com>
2025-01-08 19:19:34 -0600

    Refactor ASGI application entry point to specify operator_app type as Robyn

--- src/teamserver/packages/core/src/core/asgi.py
+++ src/teamserver/packages/core/src/core/asgi.py
@@ -3,5 +3,5 @@ from .application import create_operator_app
 
 if __name__ == "__main__":
     # create a configured "Session" class
-    operator_app = create_operator_app()
+    operator_app: Robyn = create_operator_app()
     operator_app.start(host="0.0.0.0", port=8080)
