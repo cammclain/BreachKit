41784c67211a5654d0bf16baf535416678ec83fa
Cam <cammclain@protonmail.com>
2025-01-08 19:44:45 -0600

    Add initial application modules for Rust server
    - Introduced `mod.rs` files for the main application entry point and models module, establishing the foundational structure for the Rust server application.
    - The `application/mod.rs` file initializes the server, while the `models/mod.rs` file defines data structures for Active Directory and other environments.
    - This commit enhances the server's architecture by organizing core components for future development.

---
+++ src/teamserver/packages/server/src/application/mod.rs
@@ -0,0 +1,4 @@
+// This is the main entry point for the server application.
+// It is responsible for initializing the application and starting the server.
+
+
---
+++ src/teamserver/packages/server/src/application/models/mod.rs
@@ -0,0 +1,15 @@
+// This is the models module for the server application.
+// It is responsible for defining the data structures and types used by the application.
+// It aims to represent all possible data that can be found within an Active Directory environment.
+
+// Common
+pub mod common;
+
+// Active Directory
+pub mod active_directory;
+
+// Azure AD
+pub mod azure_ad;
+
+// Windows
+pub mod windows;
