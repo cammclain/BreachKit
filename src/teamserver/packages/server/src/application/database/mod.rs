8361b314f3d1fc9676298ebeb21d0faa0477f4c6
Cam <cammclain@protonmail.com>
2025-01-08 19:46:05 -0600

    Add database module for server application
    - Introduced a new `mod.rs` file in the database module to handle database operations and connections.
    - This module provides a unified interface for all database interactions and specifies the use of SurrealDB.
    - This addition enhances the server's architecture by establishing a dedicated component for database management.

---
+++ src/teamserver/packages/server/src/application/database/mod.rs
@@ -0,0 +1,5 @@
+// This is the database module for the server application.
+// It is responsible for handling the database operations and connections.
+// It aims to provide a unified interface for all database operations.
+// NOTE: We are using SurrealDB as our database.
+
