2f3a4d318944a65e6bd33ffa479fd9071df9642b
Cam <cammclain@protonmail.com>
2025-01-09 01:15:34 -0600

    Add routers module for teamserver application
    - Introduced a new `mod.rs` file in the routers module to organize and expose router functionalities for the teamserver application.
    - The file includes placeholders for future router modules such as `auth`, `agents`, and `campaigns`, setting the stage for enhanced routing capabilities.
    - This addition improves the modular structure of the application, facilitating better management and accessibility of routing functionalities.

---
+++ src/teamserver/packages/server/src/application/routers/mod.rs
@@ -0,0 +1,12 @@
+// this file is the mod file for the routers of the teamserver
+// this file basically just pulls the routers into the module the module to make them accessible to the rest of the application
+
+//pub mod auth;
+
+// NOTE: this is a hypothetical future router that the client uses to manage and manipulate agents on the team server!
+//pub mod agents;
+
+// NOTE: this is another hypothetical future router that the client uses to interact with the campaigns in the database. add new ones, manage existing ones. etc.
+//pub mod campaigns;
+
+//pub mod <other router modules>
